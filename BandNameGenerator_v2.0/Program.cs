028ae6d9616c4f5f4f7703ebec4ea120a6df2b9a
x00143783 <x00143783@mytudublin.ie>
2024-11-22 09:43:11 +0000

    Initail commit/title page

---
+++ BandNameGenerator_v2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+// creating the random generator
+Random randy = new Random();
+
+
+Console.WriteLine("********************************");
+Console.WriteLine("*                              *");
+Console.WriteLine("*                              *");
+Console.WriteLine("*Welcome To Band Name Generator*");
+Console.WriteLine("*           V_2.0              *");
+Console.WriteLine("*                              *");
+Console.WriteLine("**********************************");
+
+
+Console.ReadKey();
